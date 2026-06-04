88a4214645001f761f717e715fb8bf8d82cc9a81
Nandini M <nanduanu27@gmail.com>
2026-06-04 22:16:40 +0530

    Added MahirlAndMath solution

---
+++ MahirlAndMath1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int target = int.Parse(Console.ReadLine());
+
+        Queue<(int value, int steps)> queue = new Queue<(int, int)>();
+        HashSet<int> visited = new HashSet<int>();
+
+        queue.Enqueue((10, 0));
+        visited.Add(10);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            int number = current.value;
+            int steps = current.steps;
+
+            if (number == target)
+            {
+                Console.WriteLine(steps);
+                return;
+            }
+
+            int[] nextNumbers =
+            {
+                number + 2,
+                number - 1,
+                number * 3
+            };
+
+            foreach (int next in nextNumbers)
+            {
+                if (next >= 0 && next <= 100000 && !visited.Contains(next))
+                {
+                    visited.Add(next);
+                    queue.Enqueue((next, steps + 1));
+                }
+            }
+        }
+    }
+}
+
---
+++ MahirlAndMath1/README.md
@@ -0,0 +1,27 @@
+# Mahirl And Math
+
+## Problem Statement
+
+Starting from 10, Mahirl can perform:
+
+- +2
+- -1
+- *3
+
+Find the minimum number of operations required to reach a target number N.
+
+## Sample Input
+
+33
+
+## Sample Output
+
+4
+
+## Technologies Used
+
+- C#
+- .NET 10
+- Queue
+- HashSet
+- Breadth First Search (BFS)
