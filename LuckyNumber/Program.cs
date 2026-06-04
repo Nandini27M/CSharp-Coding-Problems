d90d639d3df49dfcce2c2a8673e3302eb9d43604
Nandini M <nanduanu27@gmail.com>
2026-06-04 21:40:48 +0530

    Added LuckyNumber solution

---
+++ LuckyNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (int i = m; i <= n; i++)
+        {
+            if (!IsPrime(i))
+            {
+                int sum1 = SumOfDigits(i);
+                int sum2 = SumOfDigits(i * i);
+
+                if (sum2 == sum1 * sum1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+
+    static int SumOfDigits(int number)
+    {
+        int sum = 0;
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ LuckyNumber/README.md
@@ -0,0 +1,28 @@
+# Lucky Number
+
+## Problem Statement
+
+A non-prime positive integer x is called a Lucky Number if:
+
+S(x²) = S(x) × S(x)
+
+where S(n) is the sum of digits of n.
+
+Find the number of Lucky Numbers between m and n.
+
+## Sample Input
+
+20
+30
+
+## Sample Output
+
+4
+
+## Technologies Used
+
+- C#
+- .NET 10
+- Loops
+- Functions
+- Prime Number Logic
