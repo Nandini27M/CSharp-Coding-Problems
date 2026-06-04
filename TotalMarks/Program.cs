892811bfee139fc3460e9b149c60097da9e90055
Nandini M <nanduanu27@gmail.com>
2026-06-04 22:01:34 +0530

    Added TotalMarks Solution

---
+++ TotalMarks/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int X = int.Parse(Console.ReadLine());   // Marks for Type 1 question
+        int Y = int.Parse(Console.ReadLine());   // Marks for Type 2 question
+        int N1 = int.Parse(Console.ReadLine());  // Number of Type 1 questions
+        int N2 = int.Parse(Console.ReadLine());  // Number of Type 2 questions
+        int M = int.Parse(Console.ReadLine());   // Total marks scored
+
+        bool found = false;
+        int type1Correct = 0;
+        int type2Correct = 0;
+
+        // Start from maximum Type 1 questions as per requirement
+        for (int i = N1; i >= 0; i--)
+        {
+            for (int j = N2; j >= 0; j--)
+            {
+                if ((i * X) + (j * Y) == M)
+                {
+                    type1Correct = i;
+                    type2Correct = j;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+                break;
+        }
+
+        if (found)
+        {
+            Console.WriteLine("Valid");
+            Console.WriteLine(type1Correct);
+            Console.WriteLine(type2Correct);
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+}
---
+++ TotalMarks/README.md
@@ -0,0 +1,35 @@
+# Total Marks
+
+## Problem Statement
+
+There are two types of questions in an exam.
+
+- Type 1 questions carry X marks.
+- Type 2 questions carry Y marks.
+
+There are N1 questions of Type 1 and N2 questions of Type 2.
+
+Given the total score M, determine whether it is possible to obtain that score.
+
+If multiple solutions exist, display the solution with the maximum number of Type 1 questions answered correctly.
+
+## Sample Input
+
+5
+8
+5
+4
+44
+
+## Sample Output
+
+Valid
+4
+3
+
+## Technologies Used
+
+- C#
+- .NET 10
+- Loops
+- Conditional Statements
